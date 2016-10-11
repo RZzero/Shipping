57017857fb938eca380efb0193eb625ce1e71f2b
Rafael Suazo <rafaelsuazoh@gmail.com>
2016-10-11 19:37:26 -0400

    Initial Commit

---
+++ src/Shipping.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+
+public class Shipping {
+
+	
+	
+	
+	/*
+	 * Calcular el shipping
+	 */
+	public double CalcShipping(String ItemType, double price){
+		
+		double shipping = 0;
+		String item;
+		item = ItemType.toLowerCase();
+
+		
+		if(item.equals("libro") || item.equals("libro ") || item.equals(" libro") || item.equals(" libro ")){
+			
+			return shipping = 0.0;
+			
+		}
+		else if(price > 100){
+			return shipping = 5.0; 
+		}
+		else{
+			
+			shipping = (price*0.05);
+			return shipping;
+			
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		String articulo;
+		double precio = 0.0;
+		Shipping a = new Shipping();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Introduzca los datos para calcular el Shipping: ");
+		System.out.println("Articulo: " );
+		articulo = scan.nextLine();
+		System.out.println("Precio: " );
+		precio = scan.nextDouble();
+		
+		scan.close();
+		
+		System.out.println("El monto del shipping es: " + a.CalcShipping(articulo, precio));
+		
+	
+
+	}
+
+}
---
+++ src/ShippingTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShippingTest {
+
+	@Test
+	public void testCalcShipping() {
+		
+		
+		Shipping a = new Shipping();
+		double test1 = a.CalcShipping("Pc", 9);
+		double test2 = a.CalcShipping("Libro", 80);
+		double test3 = a.CalcShipping("lIbro ", 120);
+		double test4 = a.CalcShipping(" LibrO ", 60);
+		double test5 = a.CalcShipping("iPhone", 650);
+		double test6 = a.CalcShipping("Jeans", 50);
+		
+		assertEquals(0.45, test1, 0.001);
+		assertEquals(0.00, test2, 0.001);
+		assertEquals(0.00, test3, 0.001);
+		assertEquals(0.00, test4, 0.001);
+		assertEquals(5.00, test5, 0.001);
+		assertEquals(2.50, test6, 0.001);
+		
+	}
+
+}
