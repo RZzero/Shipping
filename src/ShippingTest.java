2b9406de17a25a58f10f2a1388bbc8c458ceb80d
Rafael Suazo <rafaelsuazoh@gmail.com>
2016-10-13 17:46:53 -0400

    some

--- src/ShippingTest.java
+++ src/ShippingTest.java
@@ -6,8 +6,8 @@ public class ShippingTest {
 
 	@Test
 	public void testCalcShipping() {
-		
-		
+
+
 		Shipping a = new Shipping();
 		double test1 = a.CalcShipping("Pc", 9);
 		double test2 = a.CalcShipping("Libro", 80);
@@ -15,14 +15,14 @@ public class ShippingTest {
 		double test4 = a.CalcShipping(" LibrO ", 60);
 		double test5 = a.CalcShipping("iPhone", 650);
 		double test6 = a.CalcShipping("Jeans", 50);
-		
-		assertEquals(0.45, test1, 0.001);
+
+		assertssEquals(0.45, test1, 0.001);
 		assertEquals(0.00, test2, 0.001);
 		assertEquals(0.00, test3, 0.001);
 		assertEquals(0.00, test4, 0.001);
 		assertEquals(5.00, test5, 0.001);
 		assertEquals(3.50, test6, 0.001);
-		
+
 	}
 
 }
